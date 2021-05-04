dec0ef5236d03b27a0f4cc0685b079528177e4ef
Josh Buker <crypto@joshbuker.com>
2021-05-03 23:46:02 -0700

    Use autoloading and combine ffi_engine.rb and engine.rb

--- lib/argon2.rb
+++ lib/argon2.rb
@@ -2,17 +2,22 @@
 
 ##
 # This Ruby Gem provides FFI bindings and a simplified interface to the Argon2
-# algorithm. Argon2 is the official winner of the Password Hashing Competition,
-# a several year project to identify a successor to bcrypt/PBKDF/scrypt methods
-# of securely storing passwords. This is an independent project and not official
-# from the PHC team.
+# algorithm.
 #
-module Argon2; end
-
-require 'argon2/constants'
-require 'argon2/ffi_engine'
-require 'argon2/version'
-require 'argon2/errors'
-require 'argon2/engine'
-require 'argon2/hash_format'
-require 'argon2/password'
+# Argon2 is the official winner of the Password Hashing Competition, a several
+# year project to identify a successor to bcrypt/PBKDF/scrypt methods of
+# securely storing passwords.
+#
+# This is an independent project and not official from the PHC team.
+#
+module Argon2
+  autoload :Constants,  'argon2/constants'
+  autoload :Error,      'argon2/errors'
+  autoload :Errors,     'argon2/errors'
+  autoload :ERRORS,     'argon2/errors'
+  autoload :Ext,        'argon2/engine'
+  autoload :Engine,     'argon2/engine'
+  autoload :VERSION,    'argon2/version'
+  autoload :HashFormat, 'argon2/hash_format'
+  autoload :Password,   'argon2/password'
+end
